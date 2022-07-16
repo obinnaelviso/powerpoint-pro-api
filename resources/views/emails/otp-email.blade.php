e79f1f7ffb571550fbf6a8e0e9f97b214dbd6d29
Obinna Elvis Okechukwu <dreamor47@gmail.com>
2022-07-16 09:39:32 +0100

    change email otp color to red

--- resources/views/emails/otp-email.blade.php
+++ resources/views/emails/otp-email.blade.php
@@ -2,7 +2,7 @@
 
 Here is your One-Time Password (OTP):
 <br>
-<div style="font-weight: 900; text-align: center; font-size:32px; color: blue">{{ $otp }}</div>
+<div style="font-weight: 900; text-align: center; font-size:32px; color: red">{{ $otp }}</div>
 <br>
 
 Note: This OTP is valid for only {{ $expireInMins }} minutes. Please don't disclose this code to anybody.
